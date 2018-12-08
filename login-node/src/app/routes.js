757a75009c0965f2ab5e41053d6b41909e0ce2e8
Valeria Rodriguez <valery-rod@hotmail.com>
2018-12-08 00:16:45 -0500

    Add insertion routes

--- login-node/src/app/routes.js
+++ login-node/src/app/routes.js
@@ -48,6 +48,112 @@ module.exports = (app, passport) => {
 	    res.redirect('/')
 	});
 
+	
+	//añadir dinero
+app.post("/addMoney", isLoggedIn, (res,req) =>{
+	user.findById({_id: req.body.userId}, function(user,err){
+		if(err){
+			res.status(500).send()	
+		}
+		user.account.saldoTotal = user.account.saldoTotal + parseInt(req.body.money)
+
+		user.account.saldoDisponible = user.account.saldoDisponible + parseInt(req.body.money)
+		user.save()
+		res.redirect('/profile')
+	})
+})
+
+//remover dinero
+app.post("/removeMoney", isLoggedIn, (res, req) => {
+	user.findById({_id: req.body.userId}, function(user,err){
+		user.account.saldoTotal = user.account.saldoTotal - parseInt(req.body.money)
+
+		user.account.saldoDisponible = user.account.saldoDisponible - parseInt(req.body.money)
+		user.save()
+		res.redirect('/profile')
+	})
+})
+
+//añadir dinero al colchon
+app.post("/addMattress", isLoggedIn, (res,req) => {
+	user.findById({_id: req.body.userId}, function(user,err){
+		if(err){
+			res.status(500).send()
+		}
+		user.account.mattress = user.account.mattress + parseInt(req.body.mattressMoney);
+		user.save()
+		res.redurect('/profile')
+	})
+})
+
+//añadir dinero al bolsillo
+app.post("/addPocket", isLoggedIn, (res,req) => {
+	user.findById({_id: req.body.userId}, function(user, err){
+		if(err){
+			res.status(500).send()
+		}
+		user.account.pockets.push({
+			pocketName: req.body.pocketName,
+			pocketSaldo: req.body.pocketSaldo
+		})
+		user.save()
+		res.redirect('/profile')
+	})
+})
+
+//eliminar bolsillo
+app.post("/removePocket", isLoggedIn, (res,req) => {
+	user.findById({_id: req.body.userId}, function(user, err){
+		var pockets = user.account.pockets
+		for(var i = 0; i < pockets.length; i++ ){
+			if(pockets[i] == req.body.pockedId){
+				user.account.saldoTotal += pockets[i].pocketSaldo
+			}
+		}
+		user.account.pockets.id(req.body.pocketId).remove()
+		user.save()
+		res.redirect('/profile')
+	})
+})
+
+
+//añadir una meta
+app.post("/addGoal", isLoggedIn, (res,req) => {
+	user.findById({_id: req.body.userId}, function(user,err){
+		if(err){
+			res.status(500).send()
+		}
+		user.account.goals.push({
+			goalName: req.body.goalName,
+			montoTotal: req.body.goalTotal,
+			dineroAhorrado: 0,
+			dineroRestante: req.body.goalTotal,
+			estado: false,
+			fechaLimite: req.body.goalDate
+		})
+		user.save()
+		res.redirect('/profile')
+	})
+})
+
+
+//añadir dinero a la meta
+app.post("/addGoalMoney", isLoggedIn, (res,req) => {
+	user.findById({_id: req.body.userId}, function(user,err){
+		if(err){
+			res.status(500).send()
+		}
+		var monto = parseInt(req.body.goalMoney)
+		user.account.goals.id(req.body.idGoal).dineroAhorrado += amount,
+		user.account.goals.id(req.boy.idGoal).montoTotal -= amount,
+		user.account.saldoDisponible -= amount
+		user.save()
+		res.redirect('/profile')
+	})
+})
+
+
+
 };
 
 function isLoggedIn(req,res,next){
